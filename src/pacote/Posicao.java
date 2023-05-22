6d49a8a748cc707411c5f40864c5c3c8176c0973
Pedro <trabalho.prog.pedro@gmail.com>
2023-05-22 02:09:29 -0300

    Método Principal e Posição

---
+++ src/pacote/Posicao.java
@@ -0,0 +1,46 @@
+package pacote;
+
+public class Posicao {
+	private int coluna;
+	private int linha;
+	
+	
+	
+	public Posicao(int coluna, int linha) {
+		super();
+		this.coluna = coluna;
+		this.linha = linha;
+	}
+	
+	
+	public int getColuna() {
+		return coluna;
+	}
+
+
+	public void setColuna(int coluna) {
+		this.coluna = coluna;
+	}
+
+
+	public int getLinha() {
+		return linha;
+	}
+
+
+	public void setLinha(int linha) {
+		this.linha = linha;
+	}
+	
+	@Override
+	public String toString() {
+		return coluna + " , " + linha;//printar a linha , coluna
+	}
+
+	public void Borda() {
+		
+	}
+	public void Peca() {
+		
+	}
+}
---
+++ src/pacote/Principal.java
@@ -0,0 +1,8 @@
+package pacote;
+
+public class Principal {
+	public static void main(String[] args) {
+		Posicao pos = new Posicao(3,6);
+		System.out.println(pos);
+	}
+}
